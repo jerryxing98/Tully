46d629803d23d965551cbbca7cd54d403c88b8b6
jerry_xing8@163 <jerry_xing8@163.com>
2014-02-26 09:54:02 +0800

    change the model of profile

--- ChineBlog/account/models.py
+++ ChineBlog/account/models.py
@@ -21,103 +21,7 @@ from userena.models import UserenaBaseProfile
 
 import datetime
 class ProfileManager(UserenaBaseProfileManager):
-    def is_fans(self,**kwargs):
-        username = kwargs['username']
-        try:
-            user_id = User.objects.get(username = username)
-        except:
-            user_id = None
-        ex_username = kwargs['ex_username']
-        try:
-            ex_id = User.objects.get(username = ex_username)
-        except:
-            ex_id = None
-        if ex_id and user_id:
-            try:
-                a  = Friend.objects.are_friends(user1 = user_id,user2 = ex_id)
-                return True
-            except ObjectDoesNotExist:
-                return False
-        else:
-            return False
 
-    def is_follows(self,**kwargs):
-        username = kwargs['username']
-        ex_username = kwargs['ex_username']
-        try:
-            ex_id = User.objects.get(username = ex_username)
-        except:
-            ex_id = None
-        try:
-            id = User.objects.get(username = username)
-        except:
-            id = None
-        if id and ex_id:
-            try:
-                a = Follow.objects.follows(follower = ex_id,followee = id)
-                return True
-            except ObjectDoesNotExist:
-                return False
-        else:
-            return False        
-
-
-    def fans_follows(self,**kwargs):
-        pass
-
-
-    def get_fans_count(self,**kwargs):
-        username = kwargs.get('username')
-        ex_username = kwargs.get('ex_username')  
-        if username:
-            try:
-                user = User.objects.get(username = username )
-            except:
-                user = None
-        elif ex_username:
-            try:
-                user = User.objects.get(username = ex_username )
-            except:
-                user = None
-        else:
-            return 0
-        
-        #fans_count = user.friend_set.all().count()
-        fans_count = Follow.objects.following(user).count()
-        return fans_count
-            
-    def get_follows_count(self,**kwargs):
-        username = kwargs.get('username')
-        ex_username = kwargs.get('ex_username')
-        
-        if username:
-            try:
-                user = User.objects.get(username = username )
-            except:
-                user = None
-        elif ex_username:
-            try:
-                user = User.objects.get(username = ex_username )
-            except:
-                user = None
-        else:
-            return 0
-        
-        follow_count = Follow.objects.followers(user).count()
-        #follow_count = user.to_friend_set.all().count()
-        return follow_count
-    
-
-    def get_info(self,**kwargs):
-        username = kwargs.get('username')
-        ex_username = kwargs.get('ex_username')
-        return {'fans_count':self.get_fans_count(username = ex_username),
-                'follows_count':self.get_follows_count(username = ex_username),
-                'is_follows':self.is_follows(username = username,ex_username = ex_username),
-                'is_fans':self.is_fans(username = username,ex_username = ex_username),
-                #'tag_count':Tag.objects.user_tag_counts(ex_username),
-                #'bookmark_count':Bookmark.objects.get_user_bookmark_count(ex_username),
-                }
     
     '''
     def get_fans_list(self,**kwargs):
@@ -184,6 +88,11 @@ class ProfileManager(UserenaBaseProfileManager):
         else:
             pass
 
+    def get_query_set(self):
+        
+
+        return (profile for profile in super(ProfileManager, self).get_query_set())
+
 
 
 class Profile(UserenaBaseProfile):
@@ -231,7 +140,105 @@ class Profile(UserenaBaseProfile):
             if birthday > today: return today.year - self.birth_date.year - 1
             else: return today.year - self.birth_date.year
            
-             
+     def is_fans(self,ex_username):
+        username = self.user.username
+
+        try:
+            user_id = User.objects.get(username = username)
+        except:
+            user_id = None
+        try:
+            ex_id = User.objects.get(username = ex_username)
+        except:
+            ex_id = None
+        if ex_id and user_id:
+            try:
+                a  = Friend.objects.are_friends(user1 = user_id,user2 = ex_id)
+                return True
+            except ObjectDoesNotExist:
+                return False
+        else:
+            return False
+
+    def is_follows(self,ex_username):
+        username = self.user.username
+
+        try:
+            ex_id = User.objects.get(username = ex_username)
+        except:
+            ex_id = None
+        try:
+            id = User.objects.get(username = username)
+        except:
+            id = None
+        if id and ex_id:
+            try:
+                a = Follow.objects.follows(follower = ex_id,followee = id)
+                return True
+            except ObjectDoesNotExist:
+                return False
+        else:
+            return False        
+
+
+    def fans_follows(self,ex_username):
+        pass
+
+
+    def get_fans_count(self,ex_username):
+        username = self.user.username
+
+        if username:
+            try:
+                user = User.objects.get(username = username )
+            except:
+                user = None
+        elif ex_username:
+            try:
+                user = User.objects.get(username = ex_username )
+            except:
+                user = None
+        else:
+            return 0
+        
+        #fans_count = user.friend_set.all().count()
+        fans_count = Follow.objects.following(user).count()
+        return fans_count
+            
+ 
+    def get_follows_count(self,ex_username):
+        username = self.user.username
+        
+        if username:
+            try:
+                user = User.objects.get(username = username )
+            except:
+                user = None
+        elif ex_username:
+            try:
+                user = User.objects.get(username = ex_username )
+            except:
+                user = None
+        else:
+            return 0
+        
+        follow_count = Follow.objects.followers(user).count()
+        #follow_count = user.to_friend_set.all().count()
+        return follow_count
+    
+
+    def get_info(self,ex_username):
+        username = self.user.username
+
+        return {'fans_count':self.get_fans_count(username = ex_username),
+                'follows_count':self.get_follows_count(username = ex_username),
+                'is_follows':self.is_follows(username = username,ex_username = ex_username),
+                'is_fans':self.is_fans(username = username,ex_username = ex_username),
+                #'tag_count':Tag.objects.user_tag_counts(ex_username),
+                #'bookmark_count':Bookmark.objects.get_user_bookmark_count(ex_username),
+                }            
+    
+        
 
         
     def __unicode__(self):
