797f2d41e7bc9068e1ad3519dd1ea03242382281
jerryxing98 <jerryxing98@gmail.com>
2014-03-05 21:31:48 +0800

    add the Product Admin in DjangoAdmin

--- sites/ebook/admin.py
+++ sites/ebook/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from ebook.models import Product
 
-
+'''
+	ProductModel show in djangoAdmin
+'''
 class ProductAdmin(admin.ModelAdmin):
+
     list_display        = ('title', 'thumbnail', 'created_by', 'status', 'num_views', 'num_replies', )
     search_fields       = ('title', 'created_by__username', )
     #raw_id_fields       = ('attachments', 'created_by')
