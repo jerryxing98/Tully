a75d1ce2f85b1dc0f58453fa1ca2408cb754dbbb
jerryxing98 <jerryxing98@gmail.com>
2014-03-05 21:34:54 +0800

    add the ebook setting model

--- sites/ebook/settings.py
+++ sites/ebook/settings.py
@@ -1,20 +1,22 @@
 from django.conf import settings
 gettext = lambda s: s
 
-
+#thumbnaler defailt size 
 EBOOK_THUMBNAIL_SIZE = getattr(settings,
                                'EBOOK_THUMBNAIL_SIZE',
                                140)
-
+#thumbnail crop size
 EBOOK_THUMBNAIL_CROP_TYPE = getattr(settings,
                                     'EBOOK_THUMBNAIL_CROP_TYPE',
                                     'smart')
+
 
 
 EBOOK_THUMBNAIL_PATH = getattr(settings,
 		                       'EBOOK_THUMBNAIL_PATH', 
 		                       '/thumbnail')
 
+#if the thumbnail path can't found ,use this path instead
 EBOOK_THUMBNAIL_URL = getattr(settings,
 		                       'EBOOK_THUMBNAIL_URL', 
 		                       '/static/img/tl-default-small.gif')
