e9f5edb97d5e49047fd58f4559c29d01ba18c0a3
jerryxing98 <jerryxing98@gmail.com>
2014-03-05 20:58:30 +0800

    change the project layout

--- ChineBlog/blog/static/blog/coolblue/js/theme.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Chine
- */
-
- function switchTheme(theme) {
- 	$.cookie('blog_theme', theme, { expires: 30 });
-	location.href = location.href;
- }
--- ChineBlog/local_settings.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-
-ADMINS = (
-    ('admin', 'jerry_xing8@163.com'),
-)
