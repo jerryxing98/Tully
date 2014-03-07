7b4020b98f36cc9b22a788718a81f640966c207f
jerryxing98 <jerryxing98@gmail.com>
2014-03-07 08:47:55 +0800

    add the tag page in bookmark!

--- sites/bookmark/urls.py
+++ sites/bookmark/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     #timeline all tags list
     #url(r'^tags/$', views.tags, name='bookmark_tags'),
     url(r'^bk/new/$', views.new, name='bookmark_new'),
+    url(r'^tag/(?P<tag_name>[^/]+)/$', views.tag, name='bookmark_tag'),
     url(r'^bk/(?P<pk>\d+)/delete/$', views.delete, name='bookmark_delete'),
     url(r'^bk/(?P<pk>\d+)/postcomment_/$', views.postcomment_, name='bookmark_postcomment_'),
     url(r'^bk/(?P<pk>\d+)/$', views.detail, name='bookmark_detail'),
