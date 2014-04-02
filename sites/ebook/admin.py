de9a5fe1a63e5cbbc5b218047327937e54bfd19a
jerry_xing8@163 <jerry_xing8@163.com>
2014-04-02 18:13:13 +0800

    add scraper object support

--- sites/ebook/admin.py
+++ sites/ebook/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ebook.models import Product
+from ebook.models import Product,Article,ProductWebsite
 
 '''
 	ProductModel show in djangoAdmin
@@ -10,4 +10,17 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields       = ('title', 'created_by__username', )
     #raw_id_fields       = ('attachments', 'created_by')
 
+class ArticleAdmin(admin.ModelAdmin):
+	list_display =('created_by','status')
+	search_fields = ('title','created_by__username')
+
+class ProductWebsiteAdmin(admin.ModelAdmin):
+    list_display =('name','url','scraper','scraper_runtime')
+    search_fields =('name','url')
+    
+
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductWebsite,ProductWebsiteAdmin)
+
+
