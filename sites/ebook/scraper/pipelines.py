e1ff1b1b8b1d536b610c74226ef129359a409bf2
jerry_xing8@163 <jerry_xing8@163.com>
2014-04-03 17:49:34 +0800

    change the item of scraper attr

--- sites/ebook/scraper/pipelines.py
+++ sites/ebook/scraper/pipelines.py
@@ -2,17 +2,20 @@ from django.db.utils import IntegrityError
 from scrapy import log
 from scrapy.exceptions import DropItem
 from dynamic_scraper.models import SchedulerRuntime
+from django.contrib.auth.models import User
+
 
 class DjangoWriterPipeline(object):
     
     def process_item(self, item, spider):
         try:
-            item['bookmark_site'] = spider.ref_object
+            item['article_website'] = spider.ref_object
             
             checker_rt = SchedulerRuntime(runtime_type='C')
             checker_rt.save()
             item['checker_runtime'] = checker_rt
-            
+            item['created_by']= User.objects.get(username='admin')
+
             item.save()
             spider.action_successful = True
             spider.log("Item saved.", log.INFO)
